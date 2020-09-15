6a84f4f2a80da2b4b26d6e25aab0906755fdb218
krisna firdaus <krisnafirdaus@gmail.com>
2020-09-15 15:37:31 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div>
+      <h1 className="judul">React Component</h1>
       <ListProduct />
     </div>
   );
--- src/Component/ListProduct.jsx
+++ src/Component/ListProduct.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import Card from "Component/Card";
-import Logo from "Assets/0.jpg";
+import Book from "Assets/book.jpg";
+import Pen from "Assets/pen.jpg";
+import Pencil from "Assets/pencil.jpg";
+import Eraser from "Assets/eraser.jpg";
 
 const ListProduct = () => {
   // state untuk datanya
   return (
     <div className="kartuList">
       <div className="card1">
-        <Card src={Logo} nama="buku" harga={4000} />
+        <Card src={Book} nama="buku" harga={4000} />
       </div>
 
       <div className="card2">
-        <Card src={Logo} nama="pensil" harga={2000} />
+        <Card src={Pencil} nama="pensil" harga={2000} />
       </div>
 
-      <div className="card2">
-        <Card src={Logo} nama="pensil" harga={2000} />
+      <div className="card3">
+        <Card src={Pen} nama="pulpen" harga={2500} />
       </div>
 
-      <div className="card2">
-        <Card src={Logo} nama="pensil" harga={2000} />
+      <div className="card4">
+        <Card src={Eraser} nama="penghapus" harga={1500} />
       </div>
     </div>
   );
